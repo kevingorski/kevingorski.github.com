6b4faafb711a7464ff8276ba0bf703b2431b8041
Kevin Gorski <kevin.gorski@gmail.com>
2012-12-09 16:36:17 -0700

    Less client-side markup fixing, more correct from the start

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-$(function() {
-	// Lettering.js for article titles
-	// $('.post h1').lettering();
-
-	// For styling bullet separate from text
-	$('.post li').wrapInner('<span />');
-
-	// Make headings be link-targetable
-	$('h2, h3, h4, h5, h6', '.post').wrapInner(function() {
-		return '<a name="' + $(this).text() + '" />';
-	}).append(function() {
-		return '<a class="section" href="#' + $(this).text() + '">&sect;</a>';
-	});
-});
+// Empty: all in pre-processing
